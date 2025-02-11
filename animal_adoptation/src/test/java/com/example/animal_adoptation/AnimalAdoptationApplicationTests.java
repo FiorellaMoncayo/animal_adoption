cb78fa88b88f3113d78bda381f2c62604263e02e
DavidSiles <davidsiles216@gmail.com>
2025-02-11 19:28:56 +0100

    Primer Commit

---
+++ animal_adoptation/src/test/java/com/example/animal_adoptation/AnimalAdoptationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.animal_adoptation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalAdoptationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
