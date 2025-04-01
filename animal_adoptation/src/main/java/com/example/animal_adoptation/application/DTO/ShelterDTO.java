15ff8b624a9139b7e18c8aa4d0c6f6794794f411
Fiorella <fmoncayozagaceta@gmail.com>
2025-04-01 15:55:04 +0200

    ShelterDTO done

--- animal_adoptation/src/main/java/com/example/animal_adoptation/application/DTO/ShelterDTO.java
+++ animal_adoptation/src/main/java/com/example/animal_adoptation/application/DTO/ShelterDTO.java
@@ -1,41 +1,42 @@
-//package com.example.animal_adoptation.interfaces.DTO;
-//
-//
-//import java.util.Optional;
-//
-//import com.example.animal_adoptation.domain.models.Shelter;
-//import com.fasterxml.jackson.annotation.JsonProperty;
-//import lombok.Data;
-//import lombok.AllArgsConstructor;
-//import lombok.NoArgsConstructor;
-//import lombok.Builder;
-//
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Builder
-//
-//public class ShelterDTO {
-//	@JsonProperty ("id")
-//	private Integer id;
-//
-//	@JsonProperty("name")
-//	private String name;
-//
-//	@JsonProperty("password")
-//	private String password;
-//
-//	public ShelterDTO(Shelter shelter) {
-//		Optional.ofNullable(shelter).ifPresent(s -> {
-//            this.id = s.getId();
-//            this.name = s.getName();
-//            this.password = s.getPassword();
-//		});
-//	}
-//
-//	public Shelter toModel() {
-//		return new Shelter(id, name, password);
-//	}
-//
-//}
-//
+package com.example.animal_adoptation.interfaces.DTO;
+
+import com.example.animal_adoptation.domain.models.Shelter;
+import com.example.animal_adoptation.domain.models.User;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ShelterDTO {
+
+    @JsonProperty("id")
+    private Integer id;
+
+    @JsonProperty("sheltername")
+    private String sheltername;
+
+    @JsonProperty("password")
+    private String password;
+
+    // Constructor que convierte de Shelter a ShelterDTO
+    public ShelterDTO(Shelter shelter) {
+        if (shelter != null) {
+            this.id = shelter.getId();
+            this.sheltername = shelter.getSheltername();
+            this.password = shelter.getPassword();
+        }
+    }
+
+    // Método para convertir de UserDTO a User
+    public User toModel() {
+        return new User(id, sheltername, password);
+    }
+}
+
