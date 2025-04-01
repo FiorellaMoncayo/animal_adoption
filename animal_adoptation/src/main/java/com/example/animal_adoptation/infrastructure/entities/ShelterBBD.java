35e34628f7622fcac40d5c078690a934945090cd
Fiorella <fmoncayozagaceta@gmail.com>
2025-04-01 14:26:21 +0200

    ShelterBBDD done

--- animal_adoptation/src/main/java/com/example/animal_adoptation/infrastructure/entities/ShelterBBD.java
+++ animal_adoptation/src/main/java/com/example/animal_adoptation/infrastructure/entities/ShelterBBD.java
@@ -1,17 +1,37 @@
 package com.example.animal_adoptation.infrastructure.entities;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "shelter")
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 
 public class ShelterBBD {
+	
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
-    @Column(name="name")
+    
+    @Column(name="sheltername", nullable = false)
     private String name;
-    @Column(name="password")
+    
+    @Column(name="password", nullable = false)
     private String password;
 }
