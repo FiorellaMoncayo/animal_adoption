52b843b27db268eb27676856b0585ba79b2537ee
DavidSiles <davidsiles216@gmail.com>
2025-05-22 00:07:18 +0200

    shelter can delete animal fix delete animal

--- animal_adoptation/src/main/java/com/example/animal_adoptation/rest/api/AnimalApi.java
+++ animal_adoptation/src/main/java/com/example/animal_adoptation/rest/api/AnimalApi.java
@@ -9,11 +9,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -77,8 +73,8 @@ public interface AnimalApi {
 
     @Operation(summary = "Delete an animal")
     @ApiResponse(description = "Animal deleted", responseCode = "204")
-    @ApiResponse(description = "Invalid input", responseCode = "400")
-    @PostMapping("/{id}")
+    @ApiResponse(description = "Animal not found", responseCode = "404")
+    @DeleteMapping("/{id}")
     ResponseEntity<Void> deleteAnimal(
             @Parameter(description = "ID of the animal to delete", required = true, in = ParameterIn.PATH)
             @PathVariable Integer id);
