2ddaf9937a0e628cf9af1cb037fc0770fcbb0db9
FiorellaMoncayo <fmoncayozagaceta@gmail.com>
2025-05-24 16:07:04 +0200

    AdoptionRequestDomainService DONE

--- animal_adoptation/src/main/java/com/example/animal_adoptation/domain/service/AdoptionRequestDomainService.java
+++ animal_adoptation/src/main/java/com/example/animal_adoptation/domain/service/AdoptionRequestDomainService.java
@@ -1,4 +1,15 @@
 package com.example.animal_adoptation.domain.service;
 
-public class AdoptionRequestDomainService {
+import com.example.animal_adoptation.domain.models.AdoptionRequest;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AdoptionRequestDomainService {
+    Optional<AdoptionRequest> createAdoptionRequest(AdoptionRequest request);
+    Optional<AdoptionRequest> getAdoptionRequestById(Integer id);
+    List<AdoptionRequest> getAdoptionRequestsByShelterId(Integer shelterId);
+    List<AdoptionRequest> getAdoptionRequestsByUserId(Integer userId);
+    Optional<AdoptionRequest> updateAdoptionRequestStatus(Integer requestId, String newStatus);
+    boolean deleteAdoptionRequest(Integer id);
 }
