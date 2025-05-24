9a3e79ef9c8876535ea0d2134c005a72592f996b
FiorellaMoncayo <fmoncayozagaceta@gmail.com>
2025-05-24 19:47:32 +0200

    Solicitud de adopción implementada

--- animal_adoptation/src/main/java/com/example/animal_adoptation/application/service/AdoptionRequestApplicationService.java
+++ animal_adoptation/src/main/java/com/example/animal_adoptation/application/service/AdoptionRequestApplicationService.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -49,6 +50,7 @@ public class AdoptionRequestApplicationService {
                 .animalId(createAdoptionRequestDTO.getAnimalId())
                 .shelterId(shelterId)
                 .status(AdoptionRequest.AdoptionStatus.PENDING)
+                .requestDate(LocalDateTime.now())
                 .build();
 
         try {
