f70233ae2e4d3705af67cb53374774255d678ea4
FiorellaMoncayo <fmoncayozagaceta@gmail.com>
2025-05-24 14:33:11 +0200

    AdoptionRequest DONE

--- animal_adoptation/src/main/java/com/example/animal_adoptation/domain/models/AdoptionRequest.java
+++ animal_adoptation/src/main/java/com/example/animal_adoptation/domain/models/AdoptionRequest.java
@@ -1,4 +1,30 @@
 package com.example.animal_adoptation.domain.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class AdoptionRequest {
+    private Integer id;
+    private Integer userId;
+    private Integer animalId;
+    private Integer shelterId;
+    private String status;
+    private LocalDateTime requestDate;
+
+    // Constructor para cuando se crea una nueva solicitud (sin ID y con estado inicial)
+    public AdoptionRequest(Integer userId, Integer animalId, Integer shelterId) {
+        this.userId = userId;
+        this.animalId = animalId;
+        this.shelterId = shelterId;
+        this.status = "PENDING";
+        this.requestDate = LocalDateTime.now();
+    }
 }
