8dcf40eec40d7e9ae2349f6a0a44fdae5069fd01
FiorellaMoncayo <fmoncayozagaceta@gmail.com>
2025-05-24 14:36:54 +0200

    AdoptionRequestRepository DONE

--- animal_adoptation/src/main/java/com/example/animal_adoptation/infrastructure/repositories/AdoptionRequestRepository.java
+++ animal_adoptation/src/main/java/com/example/animal_adoptation/infrastructure/repositories/AdoptionRequestRepository.java
@@ -1,4 +1,14 @@
 package com.example.animal_adoptation.infrastructure.repositories;
 
-public class AdoptionRequestRepository {
+import com.example.animal_adoptation.infrastructure.entities.AdoptionRequestBBD;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AdoptionRequestRepository extends JpaRepository<AdoptionRequestBBD, Integer> {
+    List<AdoptionRequestBBD> findByShelterId(Integer shelterId);
+
+    List<AdoptionRequestBBD> findByUserId(Integer userId);
 }
